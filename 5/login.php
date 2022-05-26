f9e1c0a1ce6e458f8e18111c67424be78b67e31c
LagunPavel <91014113+LagunPavel@users.noreply.github.com>
2022-05-27 01:42:34 +0300

    Update login.php

--- 5/login.php
+++ 5/login.php
@@ -29,7 +29,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./style.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
         <title>Login</title>
     </head>
